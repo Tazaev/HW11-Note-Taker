51abeebb673f7bf067a74ce618ff96e24e7a44db
Tazaev <tonymurtazaev@gmail.com>
2022-07-18 00:37:26 -0400

    added post route

--- server.js
+++ server.js
@@ -31,6 +31,27 @@ app.get("/api/notes/:id", (req, res) => {
   const index = req.params.id;
   res.json(notes[index]);
 });
-// post route
+// post routes
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  let noteId = SaveId();
+  newNote.id = noteId;
+
+  fs.readFile("./Develop/db/db.json", (err, notes) => {
+    if (err) throw err;
+    const notesArr = JSON.parse(notes);
+    notesArr.push(newNote);
+    // write files
+    fs.writeFile(
+      "./Develop/db/db.json",
+      JSON.stringify(notesArr, null, 2),
+      "utf8",
+      (err) => {
+        if (err) return console.log(err);
+        res.json(newNote);
+      }
+    );
+  });
+});
 
 //Delete route
