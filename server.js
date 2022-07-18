2c9a96bf6c80d20a01815335df9671d72491ff82
Tazaev <tonymurtazaev@gmail.com>
2022-07-18 00:15:10 -0400

    added comments and dependencies

---
+++ server.js
@@ -0,0 +1,16 @@
+//Dependencies
+const fs = require("fs");
+const express = require("express");
+const notes = require("./Develop/db/db.json");
+const path = require("path");
+const SaveId = require("uniqid");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Middleware
+
+// Get Routes for home/notes
+
+// post route
+
+//Delete route
