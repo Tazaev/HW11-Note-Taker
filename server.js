0451b0f8e37d729a4b846e89d447260dc5a50be4
Tazaev <tonymurtazaev@gmail.com>
2022-07-18 00:27:08 -0400

    added middleware portion

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const SaveId = require("uniqid");
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Middleware
-
+app.use(express.static("Develop/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Get Routes for home/notes
 
 // post route
